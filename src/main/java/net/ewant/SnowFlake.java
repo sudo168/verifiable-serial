4edc360be43a4f2f237eb2ec342d4b0c96821817
sudo168 <39115852+sudo168@users.noreply.github.com>
2020-04-13 19:17:23 +0800

    雪花算法

--- src/main/java/net/ewant/SnowFlake.java
+++ src/main/java/net/ewant/SnowFlake.java
@@ -12,25 +12,25 @@ public class SnowFlake {
     // 起始的时间戳(时间戳占41位)
     private final static long START_TIMESTAMP = 1586765555888L;
     // 每一部分占用的位数，就三个（共22位）
-    private int SEQUENCE_BIT = 12;// 序列号占用的位数
-    private int MACHINE_BIT = 5; // 机器标识占用的位数
-    private int DATA_CENTER_BIT = 5;// 数据中心占用的位数
+    private int SEQUENCE_BIT;// 序列号占用的位数
+    private int MACHINE_BIT; // 机器标识占用的位数
+    private int DATA_CENTER_BIT;// 数据中心占用的位数
     // 每一部分最大值
-    private long MAX_DATA_CENTER = -1L ^ (-1L << DATA_CENTER_BIT);
-    private long MAX_MACHINE = -1L ^ (-1L << MACHINE_BIT);
-    private long MAX_SEQUENCE = -1L ^ (-1L << SEQUENCE_BIT);
+    private long MAX_DATA_CENTER;
+    private long MAX_MACHINE;
+    private long MAX_SEQUENCE;
     // 每一部分向左的位移
-    private int MACHINE_LEFT_SHIFT = SEQUENCE_BIT;
-    private int DATA_CENTER_LEFT_SHIFT = SEQUENCE_BIT + MACHINE_BIT;
-    private int TIMESTAMP_LEFT_SHIFT = DATA_CENTER_LEFT_SHIFT + DATA_CENTER_BIT;
-    
+    private int MACHINE_LEFT_SHIFT;
+    private int DATA_CENTER_LEFT_SHIFT;
+    private int TIMESTAMP_LEFT_SHIFT;
+
     private long dataCenterId; // 数据中心
     private long machineId; // 机器标识
     private long sequence = 0L; // 序列号
     private long lastTimestamp = -1L;// 上一次时间戳
 
     public SnowFlake(long dataCenterId, long machineId) {
-        this(dataCenterId, DATA_CENTER_BIT, machineId, MACHINE_BIT);
+        this(dataCenterId, 5, machineId, 5);
     }
 
     public SnowFlake(long dataCenterId, int dataCenterBits, long machineId, int machineBits) {
@@ -109,7 +109,7 @@ public class SnowFlake {
         });
 
         int maxChars = 10;
-        long timeMax = ((long) Math.pow(62, maxChars) - 1) >> TIMESTAMP_LEFT_SHIFT;
+        long timeMax = ((long) Math.pow(62, maxChars) - 1) >> snowFlake.TIMESTAMP_LEFT_SHIFT;
         double maxYears = timeMax / (365.00 * 24 * 3600 * 1000);
         System.out.println("采用雪花算法与Base62生成" + maxChars + "位字符，最大使用年限：" + maxYears);
         // 要想增加使用年限，使TIMESTAMP_LEFT_SHIFT变小即可
